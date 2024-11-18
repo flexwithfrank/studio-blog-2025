f075b5d088e0036b88757297dc22e84dc41fc52a
videovulture <30363299+videovulture@users.noreply.github.com>
2024-11-18 07:53:37 -0800

    main

--- app/(website)/page.js
+++ app/(website)/page.js
@@ -3,7 +3,6 @@ import { getAllPosts } from "@/lib/sanity/client";
 
 export default async function IndexPage() {
   const posts = await getAllPosts();
-  console.log(posts)
   return <HomePage posts={posts} />;
 }
 
